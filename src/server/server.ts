0cfdaa2c6de9139039d7fcbefa47076c80f856a4
Philipp Mitterer <philipp.mitterer@e-mundo.de>
2020-12-26 18:50:08 +0100

    enable heroku - 2nd try

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,4 +1,3 @@
-import Bundler from "parcel-bundler";
 import express from "express";
 import {apiRouter} from "./api";
 import {startEventStore} from "./eventStore";
@@ -8,13 +7,15 @@ startEventStore()
 const app = express()
 app.use(express.json())
 
-const bundler = new Bundler('src/client/index.html', {})
-
 app.use('/api', apiRouter)
 
-// Let express use the bundler middleware, this will let Parcel handle every request over your express server
-app.use(bundler.middleware())
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static('dist'))
+} else {
+    const Bundler = require("parcel-bundler")
+    const bundler = new Bundler('src/client/index.html', {})
+    app.use(bundler.middleware())
+}
 
 // Listen on port 8080
 app.listen(8080)
