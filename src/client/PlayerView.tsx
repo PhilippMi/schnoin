fd56b0547bff652c57f659c474072b8d9b8bb351
Philipp Mitterer <philipp.mitterer@e-mundo.de>
2021-01-16 10:23:23 +0100

    highlight current player

--- src/client/PlayerView.tsx
+++ src/client/PlayerView.tsx
@@ -4,10 +4,11 @@ import React, {PropsWithChildren} from "react";
 export interface PlayerViewProps {
     score: number | string
     name: string
+    active: boolean
 }
 
 export function PlayerView(props: PropsWithChildren<PlayerViewProps>) {
-    return <div className="player">
+    return <div className={`player ${props.active ? 'player--active' : ''}`}>
         <div className="player__hand">
             {props.children}
         </div>
--- src/client/Table.tsx
+++ src/client/Table.tsx
@@ -26,28 +26,38 @@ function getScore(round: Round | undefined, o: Player): number | string {
 }
 
 export function Table(props: TableProps) {
+    const round = props.state.round;
+    const player = props.state.player;
 
     const opponents = props.state.opponents.map((o, i) =>
         <div className={`table__opponent table__opponent--${i + 1}`} key={i}>
-            {o && <PlayerView score={getScore(props.state.round, o)} name={o.name}>
+            {o && <PlayerView
+                score={getScore(round, o)}
+                name={o.name}
+                active={round?.currentPlayerId === o.id}
+            >
                 <HiddenHand nCards={o.nCards} rotated={i % 2 === 0}/>
             </PlayerView>}
         </div>
     )
-    const playerIds = [props.state.player.id, ...props.state.opponents.map(o => o?.id)]
-    const trumpSuit = props.state.round?.trumpSuit;
+    const playerIds = [player.id, ...props.state.opponents.map(o => o?.id)]
+    const trumpSuit = round?.trumpSuit;
     return <div className="table">
         {typeof trumpSuit === 'number' && <div className="table__trump-suit">
             <TrumpSuitIcon trumpSuit={trumpSuit} />
         </div>}
         <div className="table__player">
-            <PlayerView score={getScore(props.state.round, props.state.player)} name={props.state.player.name}>
-                <PlayerHand cards={props.state.player.cards} onSelectCard={props.onSelectCard} selectedCards={props.selectedCards} />
+            <PlayerView
+                score={getScore(round, player)}
+                name={player.name}
+                active={round?.currentPlayerId === player.id}
+            >
+                <PlayerHand cards={player.cards} onSelectCard={props.onSelectCard} selectedCards={props.selectedCards} />
             </PlayerView>
         </div>
         {opponents}
-        {props.state.round?.trick && <div className="table__trick">
-            <Trick trick={props.state.round?.trick} playerIds={playerIds} />
+        {round?.trick && <div className="table__trick">
+            <Trick trick={round?.trick} playerIds={playerIds} />
         </div>}
     </div>
 
